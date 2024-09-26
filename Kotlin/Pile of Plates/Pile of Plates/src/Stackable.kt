f50bd2696fd601ac7be373ddf29a1e31d04a5924
Calil Brandão <cbrandao2001@gmail.com>
2024-09-26 16:30:42 -0300

    Pile of plates

--- Kotlin/Pile of Plates/Pile of Plates/src/Stackable.kt
+++ Kotlin/Pile of Plates/Pile of Plates/src/Stackable.kt
@@ -2,9 +2,9 @@ interface Stackable {
 
     // Main methods                       CRUD Method
     fun stack(plate: Any?)    // Create : add a new item in the pile
-    fun wash(): Any?          // Read   : return the top item value
+    fun wash(): Any?           // Read   : return the top item value
     fun refresh(plate: Any?)    // Update : update the top item in the pile
-    fun unstack(): Any?         // Delete : erase the top item in the pile
+    fun unstack(): Any?          // Delete : erase the top item in the pile
 
     // Auxiliary methods
     fun itsEmpty(): Boolean
